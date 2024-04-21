8324fb3eeae012388ca2723dd9d031b5ae3cf310
mianger22 <test666777888999@yandex.ru>
2024-04-21 13:24:32 +0300

    Подключил собственную библиотеку

---
+++ index.js
@@ -0,0 +1,6 @@
+const toUpperCase = require('@ustinnov/text-to-upper-case');
+
+const myText = 'Добро пожаловать в совершенно новый мир!)';
+const upperCaseText = toUpperCase(myText);
+
+console.log(upperCaseText);

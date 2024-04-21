af2f86cdac0d0297967007dc0e38272266da9010
mianger22 <test666777888999@yandex.ru>
2024-04-21 13:30:06 +0300

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const toUpperCase = require('@ustinnov/text-to-upper-case');
+import { toUpperCase } from "@ustinnov/text-to-upper-case";
 
 const myText = 'Добро пожаловать в совершенно новый мир!)';
 const upperCaseText = toUpperCase(myText);
